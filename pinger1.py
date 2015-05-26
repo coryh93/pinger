4185a510061a4674ca7e39143f417c480ab1023c
Cory <cjh4066@rit.edu>
2015-05-25 20:11:11 -0700

    First revision of work

---
+++ pinger1.py
@@ -0,0 +1,20 @@
+#This is a program to ping across the entire range of IP's for a given
+#Flashnet switch, and to list the available IP addresses in both 
+#DHCP and static ranges.
+
+#Written by Cory Hinton
+#Pinger v1 
+#Revision date 5/25/15
+
+
+
+print "Welcome to the CALO Pinger!"
+networkID = raw_input('Please enter the network ID of the BB switch - ')
+
+
+print (networkID)
+
+
+
+
+
